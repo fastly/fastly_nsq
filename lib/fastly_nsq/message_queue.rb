189f8b2ea3c5a43893b29f14aa2c2e66268faea9
Tommy O'Neil <alieander@gmail.com>
2016-02-15 00:10:49 +0000

    Passthru .logger= and .logger to ruby-nsq
    Here we allow for a passthru of .logger= and .logger to the
    wistia/ruby-nsq gem so that we can override the default logger of
    Logger.new(nil). This should allow us to setup the nsq gem logger to be
    something like Rails.logger or some other logger as is needed.
    
    Changes:
    * Add two methods on MessageQueue (.logger= and .logger)
    * Support those methods on FakeMessageQueue
    * Tests for these methods

--- lib/fastly_nsq/message_queue.rb
+++ lib/fastly_nsq/message_queue.rb
@@ -8,4 +8,16 @@ require_relative 'message_queue/strategy'
 module MessageQueue
   FALSY_VALUES = [false, 0, '0', 'false', 'FALSE', 'off', 'OFF', nil]
   TRUTHY_VALUES = [true, 1, '1', 'true', 'TRUE', 'on', 'ON']
+
+  def self.logger=(logger)
+    strategy.logger = logger
+  end
+
+  def self.logger
+    strategy.logger
+  end
+
+  def self.strategy
+    Strategy.for_queue
+  end
 end
