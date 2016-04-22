57da0fbae05c6707efb6b4ad017e1042258e552a
Joshua Wehner <joshua.wehner@gmail.com>
2016-04-22 11:01:53 -0700

    Re-arrange a little
    Questions:
    * Are these candidates for shared behaviors?
    
    * Is it valuable to have these per-queue assertions when the *actual* Consumer is a mock?
    
    It seems as if we're maybe going to a lot of duplicative effort that's really only exercising that Strategy correctly toggles a constant.

--- spec/lib/fastly_nsq/message_queue/consumer_spec.rb
+++ spec/lib/fastly_nsq/message_queue/consumer_spec.rb
@@ -5,38 +5,8 @@ RSpec.describe MessageQueue::Consumer do
   let(:topic)    { 'death_star' }
   let(:consumer) { MessageQueue::Consumer.new(topic: topic, channel: channel) }
 
-  describe '#connection' do
-    describe 'when using the real queue', fake_queue: false do
-      it 'returns an instance of the queue consumer' do
-        allow(Nsq::Consumer).to receive(:new)
-
-        consumer.connect
-
-        expect(Nsq::Consumer).to have_received(:new).
-          with(
-            nsqlookupd: ENV.fetch('NSQLOOKUPD_HTTP_ADDRESS'),
-            topic: topic,
-            channel: channel,
-            ssl_context: nil,
-          ).at_least(:once)
-      end
-    end
-
-    describe 'when using the fake queue', fake_queue: true do
-      it 'returns an instance of the queue consumer' do
-        allow(FakeMessageQueue::Consumer).to receive(:new)
-
-        consumer.connect
-
-        expect(FakeMessageQueue::Consumer).to have_received(:new).
-          with(
-            nsqlookupd: ENV.fetch('NSQLOOKUPD_HTTP_ADDRESS'),
-            topic: topic,
-            channel: channel,
-            ssl_context: nil,
-          ).at_least(:once)
-      end
-    end
+  def fake_consumer
+    double 'Consumer', connection: nil, terminate: nil, pop: :popped
   end
 
   describe 'when the ENV is set incorrectly' do
@@ -47,50 +17,62 @@ RSpec.describe MessageQueue::Consumer do
     end
   end
 
-  describe '#terminate' do
-    describe 'when using the real queue', fake_queue: false do
-      it 'closes the connection' do
-        fake_consumer = double('Consumer', connection: nil, terminate: nil)
-        allow(Nsq::Consumer).to receive(:new).and_return(fake_consumer)
-
-        consumer.connect
-        consumer.terminate
-
-        expect(fake_consumer).to have_received(:terminate)
-      end
-    end
-
-    describe 'when using the fake queue', fake_queue: true do
-      it 'closes the connection' do
-        fake_consumer = double('Consumer', connection: nil, terminate: nil)
-        allow(FakeMessageQueue::Consumer).to receive(:new).and_return(fake_consumer)
-
-        consumer.connect
-        consumer.terminate
-
-        expect(fake_consumer).to have_received(:terminate)
-      end
-    end
-  end
-
-  let(:fake_consumer) { double 'Consumer', connection: nil, terminate: nil, pop: :popped }
-
   describe 'when using the real queue', fake_queue: false do
-    before(:each) { allow(Nsq::Consumer).to receive(:new).and_return(fake_consumer) }
+    before(:example) do
+      @fake_consumer = fake_consumer
+      allow(Nsq::Consumer).to receive(:new).and_return(@fake_consumer)
+      consumer.connect
+    end
 
     it 'forwards #pop to Nsq::Consumer' do
       consumer.pop
-      expect(fake_consumer).to have_received(:pop)
+      expect(@fake_consumer).to have_received(:pop)
+    end
+
+    it 'closes the connection' do
+      consumer.terminate
+
+      expect(@fake_consumer).to have_received(:terminate)
+    end
+
+    it 'instantiates the queue consumer' do
+      expect(Nsq::Consumer).to have_received(:new).
+        with(
+          nsqlookupd: ENV.fetch('NSQLOOKUPD_HTTP_ADDRESS'),
+          topic: topic,
+          channel: channel,
+          ssl_context: nil,
+        ).at_least(:once)
     end
   end
 
   describe 'when using the fake queue', fake_queue: true do
-    let(:fake_consumer) { double 'Consumer', connection: nil, terminate: nil, pop: :popped }
-    before(:each) { allow(FakeMessageQueue::Consumer).to receive(:new).and_return(fake_consumer) }
+    before(:example) do
+      @fake_consumer = fake_consumer
+     allow(FakeMessageQueue::Consumer).to receive(:new).and_return(@fake_consumer)
+     consumer.connect
+   end
 
     it 'forwards #pop to FakeMessageQueue::Consumer' do
       consumer.pop
-      expect(fake_consumer).to have_received(:pop)
+      expect(@fake_consumer).to have_received(:pop)
     end
+
+    it 'closes the connection' do
+      consumer.terminate
+
+      expect(@fake_consumer).to have_received(:terminate)
+    end
+
+    it 'instantiates the queue consumer' do
+      expect(FakeMessageQueue::Consumer).to have_received(:new).
+        with(
+          nsqlookupd: ENV.fetch('NSQLOOKUPD_HTTP_ADDRESS'),
+          topic: topic,
+          channel: channel,
+          ssl_context: nil,
+        ).at_least(:once)
+    end
+
   end
 end
