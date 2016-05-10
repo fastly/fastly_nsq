1655223e11def20a1ce110accaad08c391545390
Joshua Wehner <joshua.wehner@gmail.com>
2016-05-10 15:34:57 -0500

    Rspec hates me & I hate rspec

--- spec/lib/fastly_nsq/message_queue/consumer_spec.rb
+++ spec/lib/fastly_nsq/message_queue/consumer_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe MessageQueue::Consumer do
   let(:topic)    { 'death_star' }
   let(:consumer) { MessageQueue::Consumer.new(topic: topic, channel: channel) }
 
-  def fake_consumer
-    double 'Consumer', connection: nil, terminate: nil, pop: :popped, size: 0
-  end
-
   describe 'when the ENV is set incorrectly' do
     it 'raises with a helpful error' do
       allow(ENV).to receive(:[]).with('FAKE_QUEUE').and_return('taco')
@@ -19,7 +15,8 @@ RSpec.describe MessageQueue::Consumer do
 
   describe 'when using the real queue', fake_queue: false do
     before(:example) do
-      @fake_consumer = fake_consumer
+      @fake_consumer = spy 'Consumer backend'
+      consumer.instance_variable_set("@connection", nil)
       allow(Nsq::Consumer).to receive(:new).and_return(@fake_consumer)
     end
 
@@ -28,12 +25,7 @@ RSpec.describe MessageQueue::Consumer do
       expect(@fake_consumer).to have_received(:pop)
     end
 
-    it 'forwards #size to Nsq::Consumer' do
-      consumer.size
-      expect(@fake_consumer).to have_received(:size)
-    end
-
-    it 'forwards #terminate to Nsq::Consumer' do
+    it 'closes the connection' do
       consumer.terminate
 
       expect(@fake_consumer).to have_received(:terminate)
@@ -42,7 +34,8 @@ RSpec.describe MessageQueue::Consumer do
 
   describe 'when using the fake queue', fake_queue: true do
     before(:example) do
-      @fake_consumer = fake_consumer
+      @fake_consumer = spy 'Consumer backend'
+      consumer.instance_variable_set("@connection", nil)
       allow(FakeMessageQueue::Consumer).to receive(:new).and_return(@fake_consumer)
     end
 
@@ -51,12 +44,7 @@ RSpec.describe MessageQueue::Consumer do
       expect(@fake_consumer).to have_received(:pop)
     end
 
-    it 'forwards #size to FakeMessageQueue::Consumer' do
-      consumer.size
-      expect(@fake_consumer).to have_received(:size)
-    end
-
-    it 'forwards #terminate to FakeMessageQueue::Consumer' do
+    it 'closes the connection' do
       consumer.terminate
 
       expect(@fake_consumer).to have_received(:terminate)
