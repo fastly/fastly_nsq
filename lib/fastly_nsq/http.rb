6b2bc7afa0db2bbfd8fda16397ff99b303c3bc49
Tommy O'Neil <alieander@gmail.com>
2017-11-27 14:23:15 -0800

    allow cert key filenames to be passed

--- lib/fastly_nsq/http.rb
+++ lib/fastly_nsq/http.rb
@@ -5,8 +5,10 @@ require 'fastly_nsq/http/nsqd'
 require 'fastly_nsq/http/nsqlookupd'
 
 class FastlyNsq::Http
-  def initialize(uri:)
-    @uri = uri
+  def initialize(uri:, cert_filename: ENV['NSQ_SSL_CERTIFICATE'], key_filename: ENV['NSQ_SSL_KEY'])
+    @uri = uri.is_a?(URI) ? uri : URI.parse(uri)
+    @cert_filename = cert_filename
+    @key_filename = key_filename
   end
 
   def get(data = nil)
@@ -26,7 +28,7 @@ class FastlyNsq::Http
   end
 
   def use_ssl
-    return if ENV['NSQ_SSL_CERTIFICATE'].nil? || ENV['NSQ_SSL_KEY'].nil?
+    return unless can_use_ssl?
     http.use_ssl = true
     http.cert = cert
     http.key = key
@@ -35,17 +37,21 @@ class FastlyNsq::Http
 
   private
 
-  attr_reader :uri
+  attr_reader :cert_filename, :key_filename, :uri
 
   def http
     @http ||= Net::HTTP.new(uri.host, uri.port)
   end
 
+  def can_use_ssl?
+    !(cert_filename.nil? || key_filename.nil?)
+  end
+
   def cert
-    @cert ||= OpenSSL::X509::Certificate.new(File.read(ENV['NSQ_SSL_CERTIFICATE']))
+    @cert ||= OpenSSL::X509::Certificate.new(File.read(cert_filename))
   end
 
   def key
-    @key ||= OpenSSL::PKey::RSA.new(File.read(ENV['NSQ_SSL_KEY']))
+    @key ||= OpenSSL::PKey::RSA.new(File.read(key_filename))
   end
 end
