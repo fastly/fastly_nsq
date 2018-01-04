cbee098d6791f9d434ce06dca03a3de34fa02146
Joshua Lane <me@joshualane.com>
2018-01-03 16:48:11 -0800

    test: tweak PriorityThreadPool spec again
    Use a single thread but expand the timeout to 30 seconds.  This only
    seems to be an issue on CI and likely is due to resource contraints.

--- spec/priority_thread_pool_spec.rb
+++ spec/priority_thread_pool_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe FastlyNsq::PriorityThreadPool do
-  let!(:pool) { described_class.new(max_threads: 20) }
+  let!(:pool) { described_class.new(max_threads: 1) }
 
   after { pool.shutdown || pool.wait_for_termination }
 
@@ -11,7 +11,7 @@ RSpec.describe FastlyNsq::PriorityThreadPool do
     actual = []
     count = 100
     count.times { |i| pool.post(i) { actual << i } }
-    expect { pool.completed_task_count }.to eventually(eq(count)).within(5)
+    expect { pool.completed_task_count }.to eventually(eq(count)).within(30)
 
     # weak but stable assertion that the last element was not the last processed
     expect(actual.last).not_to eq(count - 1)
